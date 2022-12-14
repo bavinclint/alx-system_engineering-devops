2e38c591e4d014fd367fddac58bcd9c0af4719bc
bavinclint <bavinclint@gmail.com>
2022-12-14 18:08:32 +0300

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
-# Install puppet-lint package
-package { 'puppet-lint':
-  ensure  => '2.1.1'
+# Installing flask using puppet
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
