5593bd563207a0d7d2c5048b2a5a3100f10370b3
bavinclint <bavinclint@gmail.com>
2023-01-17 01:13:43 +0300

    Updated export

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,25 +1,73 @@
 #!/usr/bin/python3
-# csv exported
-
+"""
+    that, using this REST API, for a given employee ID,
+    returns information about his/her TODO list progress
+"""
 import csv
-from requests import get
-from sys import argv
+import json
+import requests
+import sys
+
+domain_name = "https://jsonplaceholder.typicode.com"
 
 
-def cvsWrite(user):
-    """writes to csv"""
-    data = get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-        user)).json()
-    name = get('https://jsonplaceholder.typicode.com/users/{}'.format(
-        user)).json().get('username')
-    employ_data = open('{}.csv'.format(user), 'w')
-    cwrite = csv.writer(employ_data, quoting=csv.QUOTE_ALL)
-    for line in data:
-        lined = [line.get('userId'), name,
-                 line.get('completed'), line.get('title')]
-        cwrite.writerow(lined)
-    employ_data.close()
+def get_employee_information(employee_id: int) -> json:
+    """
+        get employee informations
+    """
+    try:
+        response = requests.get(
+            url="{}/users/{}/".format(domain_name, employee_id))
+
+        return response.json()
+    except Exception:
+        return False
+
+
+def get_employee_tasks(employee_id: int) -> json:
+    """
+        get employee task list
+    """
+    try:
+        response = requests.get(
+            url="{}/users/{}/todos".format(domain_name, employee_id))
+
+        return response.json()
+
+    except Exception:
+        return False
+
+
+def write_csv_row(employee_id: int, writer):
+    """
+        write csv row
+    """
+    employee_infos = get_employee_information(employee_id)
+    employee_tasks = get_employee_tasks(employee_id)
+
+    for task in employee_tasks:
+        writer.writerow(
+            [
+                employee_id,
+                employee_infos.get("username"),
+                task.get("completed"),
+                task.get("title")
+            ]
+        )
+
+
+def export_employee_tasks_in_csv(employee_id: int):
+    """
+        export employee task list in csv
+    """
+    with open("{}.csv".format(employee_id), mode='w') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL, lineterminator='\n')
+        write_csv_row(employee_id, writer)
+    file.closed
 
 
 if __name__ == "__main__":
-    cvsWrite(argv[1])
+    if len(sys.argv) != 2:
+        raise ValueError("Usage 1-export_to_CSV.py <USERID>")
+
+    export_employee_tasks_in_csv(sys.argv[1])
