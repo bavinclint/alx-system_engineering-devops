bd1cd0036ae1836aabc70f8a5a187bb725838e33
bavinclint <bavinclint@gmail.com>
2023-02-02 11:54:17 +0300

    Count it!

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,45 +1,33 @@
 #!/usr/bin/python3
-# get subs
+"""queries the Reddit API, parses the title of all hot articles,
+and prints a sorted count of given keywords"""
 from requests import get
-from sys import argv
-
-hotlist = []
-after = None
 
 
-def count_all(hotlist, word_list):
-    count_dic = {word.lower(): 0 for word in word_list}
-    for title in hotlist:
-        words = title.split(' ')
-        for word in words:
-            if count_dic.get(word) is not None:
-                count_dic[word] += 1
-
-    for key in sorted(count_dic, key=count_dic.get, reverse=True):
-        if count_dic.get(key):
-            for thing in word_list:
-                if key == thing.lower():
-                    print("{}: {}".format(thing, count_dic[key]))
-
-
-def count_words(subreddit, word_list):
-    global hotlist
-    global after
-    """subs"""
-    head = {'User-Agent': 'Dan Kazam'}
-    if after:
-        count = get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
-            subreddit, after), headers=head).json().get('data')
-    else:
-        count = get('https://www.reddit.com/r/{}/hot.json'.format(
-            subreddit), headers=head).json().get('data')
-    hotlist += [dic.get('data').get('title').lower()
-                for dic in count.get('children')]
-    after = count.get('after')
-    if after:
-        return count_words(subreddit, word_list)
-    return count_all(hotlist, word_list)
-
-
-if __name__ == "__main__":
-    count_words(argv[1], argv[2].split(' '))
+def count_words(subreddit, word_list, count_list={}, after=""):
+    """"""
+    if count_list == {}:
+        count_list = {word.lower(): 0 for word in word_list}
+    url = "https://api.reddit.com/r/{}/hot?limit=10".format(subreddit)
+    headers = {"User-Agent": "reddit API"}
+    data = get(url, headers=headers, allow_redirects=False)
+    if data.status_code is not 200:
+        return None
+    for item in data.json().get('data').get('children'):
+        var = item.get('data').get('title')
+        for key, value in count_list.items():
+            count_list[key] += var.lower().split(' ').count(key)
+    after = data.json().get('data').get('after')
+    if after is None:
+        if all(value == 0 for value in count_list.values()):
+            return None
+        count_list = sorted(
+            count_list.items(),
+            key=lambda list: list[1],
+            reverse=True)
+        for key, value in count_list:
+            if value == 0:
+                continue
+            print("{}: {}".format(key, value))
+        return True
+    return count_words(subreddit, word_list, count_list, after)
